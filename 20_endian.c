41ba58c41c5ec8dfa1d311a96b7301622d8590cf
sushmabs15 <112747520+sushmabs15@users.noreply.github.com>
2022-09-13 09:15:10 +0530

    Update 20_endian.c

--- 20_endian.c
+++ 20_endian.c
@@ -1,11 +1,12 @@
-#include<stdio.h>
-int main(){
-	unsigned int num = 0x12345678;
-	unsigned int ret = num & 0xf;
-	if(ret == 0x78){
-		printf("the number is Big Endian");
-	}
-	else{
-		printf("the number is Little Endian");
-	}
+#include <stdio.h>
+int main()
+{
+   unsigned int i = 1;
+   char *c = (char*)&i;
+   if (*c)   
+       printf("Little endian");
+   else
+       printf("Big endian");
+   getchar();
+   return 0;
 }
